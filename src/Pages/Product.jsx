e03a761aa87925a455ce5c60eb8cfe347e69f67a
zzinue <zzinue@gmail.com>
2024-02-08 09:33:05 -0600

    adding product display and breadcrum

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
+import { ShopContext } from "../Context/ShopContex";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 
 const Product = () => {
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
   return (
-    <div>Product</div>
-  )
-}
+    <div>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+    </div>
+  );
+};
 
-export default Product
+export default Product;
