5e661af7082db78a88468ca5f483a3bb978c3870
zzinue <zzinue@gmail.com>
2024-03-03 16:54:44 -0600

    desing cart style

--- src/Context/ShopContex.jsx
+++ src/Context/ShopContex.jsx
@@ -1,23 +1,26 @@
 import React, { createContext, useState } from "react";
 import all_product from "../Components/Assets/all_product";
+
 export const ShopContext = createContext(null);
 
+const getDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < all_product.length + 1; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
+
 const ShopContextProvider = (props) => {
-  const getDefaultCard = () => {
-    let cart = {};
-    for (let index = 0; index < all_product.length + 1; index++) {
-      cart[index] = 0;
-    }
-    return cart;
-  };
-  const [cartItems, setCartItems] = useState(getDefaultCard());
+  const [cartItems, setCartItems] = useState(getDefaultCart());
 
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-    console.log(cartItems);
   };
+  console.log(cartItems);
   const removeFromCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    // console.log(cartItems);
   };
   const contextValue = { all_product, cartItems, addToCart, removeFromCart };
 
