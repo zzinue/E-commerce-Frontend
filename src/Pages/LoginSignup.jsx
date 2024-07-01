19f46903ef5e85eb926a9ade8a4bb53cbf7d01db
zzinue <zzinue@gmail.com>
2024-06-30 22:08:05 -0600

    login and sing up logic

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CSS/LoginSignup.css";
 const LoginSignup = () => {
+  const [state, setState] = useState("Login");
+
   return (
     <div className="loginsignup">
-      <div className="loginsingup-container">
-        <h1>Sign up</h1>
+      <div className="loginsignup-container">
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder="your name" />
+          {state === "Sign Up" ? (
+            <input type="text" placeholder="your name" />
+          ) : (
+            <></>
+          )}
           <input type="email" placeholder="Email Adress" />
           <input type="password" placeholder="Password" />
         </div>
         <button>Continue</button>
-        <p className="loginsignup-login">
-          Already have an account ?<span>Login here</span>
-        </p>
+
+        {state === "Sign Up" ? (
+          <p className="loginsignup-login">
+            Already have an account ?
+            <span
+              onClick={() => {
+                setState("Login");
+              }}
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p className="loginsignup-login">
+            Create an account{" "}
+            <span
+              onClick={() => {
+                setState("Sign Up");
+              }}
+            >
+              Click here
+            </span>
+          </p>
+        )}
+
         <div className="loginsignup-agree">
           <input type="checkbox" name="" id="" />
           <p>By continuing, i agree to the terms of use & privacy</p>
